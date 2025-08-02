5da684d39d28614a9fbca88766383278757bcfd0
reach2nimit <reach2nimit@gmail.com>
2025-08-03 00:02:16 +0530

    Time: 173 ms (63.88%), Space: 11.3 MB (99.03%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    
+    int countPrimes(int n) {
+        
+        bool prime[n+1];
+        
+        memset(prime, true, sizeof(prime));
+        
+        for(int p=2; p*p<n; p++){
+            
+            if(prime[p]){
+                
+                for(int i = p*p; i<n; i+=p)
+                    prime[i]=false;
+            }
+        }
+        
+        int count=0;
+        
+        for(int i = 2; i<n;i++)
+            if(prime[i])
+                count++;
+        
+        return count;
+    }
+};
