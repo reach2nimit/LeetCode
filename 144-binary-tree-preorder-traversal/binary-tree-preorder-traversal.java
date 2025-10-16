0a06d0748a296cd0c147f89366fdef29d22b750a
reach2nimit <reach2nimit@gmail.com>
2025-10-16 22:33:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (84.07%) - LeetSync

---
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> preorderTraversal(TreeNode root) { List<Integer> result = new ArrayList<>();
+        if(root == null)
+            return result;
+        // //recursive approach
+        // dfs(root, result);
+
+        //Iterative method
+        Stack<TreeNode> treeStack = new Stack<>();
+        treeStack.push(root);
+
+        while(!treeStack.isEmpty()){
+            TreeNode node = treeStack.pop();
+            result.add(node.val);
+
+            if(node.right != null)
+                treeStack.push(node.right);
+            
+            if(node.left != null)
+                treeStack.push(node.left);
+        }
+
+        return result;
+    }
+
+    private void dfs(TreeNode root, List<Integer> result){
+
+        if(root == null)
+            return;
+        
+        result.add(root.val);
+        dfs(root.left, result);
+        dfs(root.right, result);
+    }
+}
