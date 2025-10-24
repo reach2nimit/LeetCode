b2c680061ee88c4749a999d59dc7504b69c14ca1
reach2nimit <reach2nimit@gmail.com>
2025-10-24 23:50:33 +0530

    Time: 20 ms (40.81%), Space: 19.5 MB (100%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> mp;
+        
+        for(int i=0;i<strs.size();i++){
+            
+            string temp = strs[i];
+            sort(strs[i].begin(),strs[i].end());
+            mp[strs[i]].push_back(temp);
+        }
+        for(auto it = mp.begin();it!=mp.end();it++){
+            ans.push_back(it->second);
+        }
+        return ans;
+    }
+};
