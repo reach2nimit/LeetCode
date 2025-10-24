e63a341df0195674a32159dbf83fe02e97408d25
reach2nimit <reach2nimit@gmail.com>
2025-10-24 23:52:37 +0530

    Time: 6 ms (98.57%) | Memory: 47.8 MB (71.18%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -4,16 +4,19 @@ class Solution {
 
         for(String str : strs){
 
-            int[] count = new int[26];
+            // int[] count = new int[26];
 
-            for(char ch : str.toCharArray())
-                count[ch-'a']++;
+            // for(char ch : str.toCharArray())
+            //     count[ch-'a']++;
             
-            StringBuilder sb = new StringBuilder();
-            for(int num : count)
-                sb.append(num).append("#");
-            String key = sb.toString();
+            // StringBuilder sb = new StringBuilder();
+            // for(int num : count)
+            //     sb.append(num).append("#");
+            // String key = sb.toString();
 
+            char[] count = str.toCharArray();
+            Arrays.sort(count);
+            String key = new String(count);
             if(!countMap.containsKey(key))
                 countMap.put(key, new ArrayList<>());
             
