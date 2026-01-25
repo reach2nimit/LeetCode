5b1f9cf6a38670c74a1b00c66a4e29e8cd95bf21
reach2nimit <reach2nimit@gmail.com>
2026-01-25 17:01:25 +0530

    Time: 8 ms (57.87%) | Memory: 46.8 MB (13.23%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> freqMap = new HashMap();
+        for(String word : words)
+            freqMap.put(word, freqMap.getOrDefault(word, 0) + 1);
+        
+        PriorityQueue<String> minHeap = new PriorityQueue<>((a,b) -> {
+            int freqA = freqMap.get(a);
+            int freqB = freqMap.get(b);
+            if(freqA != freqB)
+                return freqA - freqB;
+            return b.compareTo(a);
+        });
+    
+        for(String word : freqMap.keySet()){
+            minHeap.offer(word);
+            if(minHeap.size() > k)
+                minHeap.poll();
+        }
+
+        List<String> result = new ArrayList();
+        while(!minHeap.isEmpty())
+            result.add(minHeap.poll());
+        
+        Collections.reverse(result);
+        return result;
+    }
+}
