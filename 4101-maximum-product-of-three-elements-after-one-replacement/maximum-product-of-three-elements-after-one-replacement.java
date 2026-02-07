5989d3b7d4e531e4e85172506b3db8f7d4e7b378
reach2nimit <reach2nimit@gmail.com>
2026-02-07 11:13:53 +0530

    Time: 5 ms (78.13%) | Memory: 97.4 MB (31.25%) - LeetSync

---
+++ 4101-maximum-product-of-three-elements-after-one-replacement/maximum-product-of-three-elements-after-one-replacement.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long maxProduct(int[] nums) {
+        long ans, max1 = -1, max2 = -1;
+        for(int i = 0; i < nums.length; i++) {
+            nums[i] = Math.abs(nums[i]);
+            if(nums[i] > max1) {
+                max2 = max1;
+                max1 = nums[i];
+            }
+            else if(nums[i] > max2) max2 = nums[i];
+        }
+        ans = max1 * max2 * 100000;
+        return ans;
+    }
+}
