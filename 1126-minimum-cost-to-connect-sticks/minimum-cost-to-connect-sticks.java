071a47856b7021202a8662fa8ae6f08bb9f79950
reach2nimit <reach2nimit@gmail.com>
2026-02-16 21:43:25 +0530

    Time: 73 ms (13.21%) | Memory: 47.9 MB (36.56%) - LeetSync

--- 1126-minimum-cost-to-connect-sticks/minimum-cost-to-connect-sticks.java
+++ 1126-minimum-cost-to-connect-sticks/minimum-cost-to-connect-sticks.java
@@ -4,11 +4,11 @@ class Solution {
         PriorityQueue<Integer> pq = new PriorityQueue();
         int totalCost = 0;
         for(int stick : sticks)
-            pq.add(stick);
+            pq.offer(stick);
         
         while(pq.size()>1){
-           int stick1 = pq.remove();
-           int stick2 = pq.remove();
+           int stick1 = pq.poll();
+           int stick2 = pq.poll();
            int cost = stick1 + stick2;
            totalCost += cost;
            pq.add(cost); 
