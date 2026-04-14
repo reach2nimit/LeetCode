2d1275e54f42fe51d5145408372d666d9a463ecb
reach2nimit <reach2nimit@gmail.com>
2026-04-14 10:48:22 +0530

    Time: 31 ms (53.43%) | Memory: 146.9 MB (52.94%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,30 +1,20 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> dq = new ArrayDeque<>();
         int[] result = new int[nums.length - k + 1];
-        int index = 0;
+        Deque<Integer> dq = new ArrayDeque();
+        int index =0;
+        for(int i = 0; i <nums.length; i++){
 
-        for(int i =0; i<k ; i++){
-            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i])
-                dq.pollLast();
-            dq.offerLast(i);
+            while(!dq.isEmpty() && nums[dq.peekLast()]< nums[i])
+                dq.removeLast();
+            dq.add(i);
+            if(dq.peekFirst() + k == i)
+                dq.removeFirst();
+            if(i>=k-1){
+                result[index] = nums[dq.peekFirst()];
+                index++;
+            }
         }
-        result[index] = nums[dq.peekFirst()];
-        index++;
-
-        for(int i = k ;i <nums.length; i++){
-            while(!dq.isEmpty() && dq.peekFirst() <= i - k)
-                dq.pollFirst();
-            
-            while(!dq.isEmpty() && nums[dq.peekLast()] <= nums[i])
-                dq.pollLast();
-            
-            dq.offerLast(i);
-            result[index] = nums[dq.peekFirst()];
-            index++;
-
-        }
-
         return result;
     }
 }
